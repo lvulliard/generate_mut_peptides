20b162096df96d4e4c07164433951213047c0106
Vulliard Loan <lv3895@neuf.fr>
2017-05-09 17:25:51 +0200

    Read and store the peptides to mutate

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # generate_mut_peptides
-Generate mutated peptides in fasta format
+##Generate mutated peptides in fasta format
 Usage:
+```
   generate_mut_peptides.py <nbSubstitutions> [-p <refPeptidome>]
   generate_mut_peptides.py --help
-
+```
 Options:
+```
   -p --peptidome     Reference peptidome to be mutated (fasta format) 
   -h --help          Show this screen.
+```
--- generate_mut_peptides.py
+++ generate_mut_peptides.py
@@ -3,7 +3,7 @@
 ##################################### Help ####################################
 """Generate mutated peptides in fasta format
 Usage:
-  generate_mut_peptides.py <nbSubstitutions> [-p <refPeptidome>]
+  generate_mut_peptides.py <nbSubstitutions> -p <refPeptidome>
   generate_mut_peptides.py --help
 
 Options:
@@ -19,6 +19,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq, MutableSeq
 from collections import defaultdict
 from Bio.SeqIO.FastaIO import SimpleFastaParser
+from Bio.Alphabet import generic_protein
 
 ################################## Functions ##################################
 
@@ -28,15 +29,16 @@ class peptide:
 
 	def __init__(self):
 		self.nbSub = 0 # Number of substitutions to perform
-		self.normPep = Seq("") # Normal peptide
-		self.mutPep = MutableSeq("") # Reference peptide
+		self.normPep = Seq("", generic_protein) # Normal peptide
+		self.mutPep = MutableSeq("", generic_protein) # Reference peptide
 
 	def addSub(self):
 		self.nbSub += 1
 
 	def setPep(self, pep):
-		self.normPep = self.normPep + pep
-		self.mutPep = self.mutPep + pep
+		pep.seq.alphabet = generic_protein
+		self.normPep = self.normPep + pep.seq
+		self.mutPep = self.mutPep + pep.seq
 
 	def __str__(self):
 		return(str(self.nbSub)+"\n"+str(self.normPep)+"\n"+str(self.mutPep))
@@ -60,4 +62,10 @@ pepDictIndices = np.random.randint(nbPep, size=nbSubstitutions)
 pepDict = defaultdict(peptide)
 for i in pepDictIndices:
 	pepDict[i].addSub()
+
+# Read and store these peptides
+pepDictIndices = set(pepDictIndices)
+for pep, i in zip(SeqIO.parse(arguments["<refPeptidome>"], "fasta"), xrange(nbPep)):
+	if i in pepDictIndices:
+		pepDict[i].setPep(pep)
 
